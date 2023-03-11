0caab0cef26f9af001917438e90bfb0771c041ef
Serge <smdyachenko@gmail.com>
2023-03-11 19:49:40 +0200

    types

--- vue/types.d.ts
+++ vue/types.d.ts
@@ -1,3 +1,2 @@
 export {DbRecord} from '../common/types'
-export {Col} from '../smart-panel/classes/Col'
-export {Fk} from '../smart-panel/classes/Fk'
+export {Col, IFk, IM2M} from '../sp-core/types'
